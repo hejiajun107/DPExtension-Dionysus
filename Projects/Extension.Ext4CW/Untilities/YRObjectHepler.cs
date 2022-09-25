b9c9e7facdb10bf9ec9807c93c6eb5d0ff26d079
hejiajun107 <hejiajun107@126.com>
2022-09-25 21:48:33 +0800

    FLHHelper

---
+++ Projects/Extension.Ext4CW/Untilities/YRObjectHepler.cs
@@ -0,0 +1,22 @@
+﻿using PatcherYRpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension.Ext4CW.Untilities
+{
+    public static class YRObjectHepler
+    {
+        public static CoordStruct ToCoordStruct(this SingleVector3D vector3D)
+        {
+            return new CoordStruct((int)vector3D.X, (int)vector3D.Y, (int)vector3D.Z);
+        }
+
+        public static SingleVector3D ToSingleVector3D(this CoordStruct coord)
+        {
+            return new SingleVector3D(coord.X, coord.Y, coord.Z);
+        }
+    }
+}
