ad537013b73aed93327d1f4af154faa155f9348f
hejiajun107 <hejiajun107@126.com>
2022-10-06 13:01:02 +0800

    没什么用

---
+++ Projects/Scripts/Japan/BunkerWallScript.cs
@@ -0,0 +1,28 @@
+﻿using DynamicPatcher;
+using Extension.Ext;
+using Extension.Script;
+using PatcherYRpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Japan
+{
+    [ScriptAlias(nameof(BunkerWallScript))]
+    [Serializable]
+    public class BunkerWallScript : TechnoScriptable
+    {
+        public BunkerWallScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        public override void OnPut(CoordStruct coord, Direction faceDir)
+        {
+            var building = Owner.OwnerObject.Convert<BuildingClass>();
+            //Logger.Log("wall:" + building.Ref.GetFWFlags());
+        }
+    }
+}
+ 
