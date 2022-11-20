cab43256a47e4b9ce22815b2472ba7c5d9aabb80
hejiajun107 <hejiajun107@126.com>
2022-11-20 12:39:33 +0800

    空袭瞄准载具的脚本

---
+++ Projects/Scripts/AirStrikeSetTargetScript.cs
@@ -0,0 +1,36 @@
+﻿using Extension.Ext;
+using Extension.Script;
+using PatcherYRpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts
+{
+    [Serializable]
+    [ScriptAlias(nameof(AirStrikeSetTargetScript))]
+    public class AirStrikeSetTargetScript : TechnoScriptable
+    {
+        public AirStrikeSetTargetScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        public override void OnUpdate()
+        {
+            if(!Owner.OwnerObject.Ref.Target.IsNull)
+            {
+                if(Owner.OwnerObject.Ref.Target.Ref.WhatAmI() == PatcherYRpp.AbstractType.Cell)
+                {
+                    var pCell = Owner.OwnerObject.Ref.Target.Convert<CellClass>();
+                    var techno = pCell.Ref.FindTechnoNearestTo(new Point2D(128, 128), false, Owner.OwnerObject);
+                    if (!techno.IsNull)
+                    {
+                        Owner.OwnerObject.Ref.SetTarget(techno.Convert<AbstractClass>());
+                    }
+                }
+            }
+        }
+    }
+}
