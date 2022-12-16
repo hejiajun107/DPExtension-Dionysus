09dc143bfe975b210135ddc04fcc41107f0415cf
hejiajun107 <hejiajun107@126.com>
2022-12-16 20:06:04 +0800

    魔法条

--- Projects/Scripts/Shared/ManaCounter.cs
+++ Projects/Scripts/Shared/ManaCounter.cs
@@ -1,7 +1,9 @@
 ﻿using DynamicPatcher;
 using Extension.Ext;
+using Extension.Utilities;
 using PatcherYRpp;
 using System;
+using System.Reflection;
 
 namespace Extension.Shared
 {
@@ -11,6 +13,7 @@ namespace Extension.Shared
         public ManaCounter(int rof = 10)
         {
             recoverDuration = rof;
+            pAnim = new SwizzleablePointer<AnimClass>(IntPtr.Zero);
         }
 
         private int max = 100;
@@ -24,11 +27,11 @@ namespace Extension.Shared
 
         private int manaCheckRof = 0;
 
-        static Pointer<BulletTypeClass> pBulletType => BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
-        static Pointer<WarheadTypeClass> pWH => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("SKILLPOWEREDWH");
+        //static Pointer<BulletTypeClass> pBulletType => BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
+        //static Pointer<WarheadTypeClass> pWH => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("SKILLPOWEREDWH");
 
-        static Pointer<WarheadTypeClass> pWH1 => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("MANAPIPWh1");
-        static Pointer<WarheadTypeClass> pWH2 => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("MANAPIPWh2");
+        //static Pointer<WarheadTypeClass> pWH1 => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("MANAPIPWh1");
+        //static Pointer<WarheadTypeClass> pWH2 => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("MANAPIPWh2");
 
         //static Pointer<WarheadTypeClass> pWH1 => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("EnerygyBarWH1");
         //static Pointer<WarheadTypeClass> pWH2 => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("EnerygyBarWH2");
@@ -36,11 +39,34 @@ namespace Extension.Shared
         //static Pointer<WarheadTypeClass> pWH4 => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("EnerygyBarWH4");
         //static Pointer<WarheadTypeClass> pWH5 => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("EnerygyBarWH5");
 
+        static Pointer<AnimTypeClass> manaAnim => AnimTypeClass.ABSTRACTTYPE_ARRAY.Find("ManaBarAnim");
 
+
+        private bool inited = false;
+
+        private SwizzleablePointer<AnimClass> pAnim;
 
 
         public void OnUpdate(TechnoExt owner)
         {
+            if(pAnim.IsNull)
+            {
+                var anim = YRMemory.Create<AnimClass>(manaAnim, owner.OwnerObject.Ref.Base.Base.GetCoords());
+                anim.Ref.SetOwnerObject(owner.OwnerObject.Convert<ObjectClass>());
+                pAnim = anim;
+            }
+
+            var visible = false;
+
+            if (owner.OwnerObject.Ref.Owner == HouseClass.Player)
+            {
+                if(owner.OwnerObject.Ref.Base.IsSelected)
+                {
+                    visible = true;
+                }
+            }
+
+
             if (currentMana < max)
             {
                 if (currentIntervel < recoverDuration)
@@ -58,12 +84,16 @@ namespace Extension.Shared
                 }
             }
 
-            if (manaCheckRof-- > 0)
-            {
-                return;
-            }
-            manaCheckRof = 15;
-            RefreshBar(owner);
+            pAnim.Ref.Animation.Value = (currentMana / 5);
+            pAnim.Ref.Pause();
+            pAnim.Ref.Invisible = !visible;
+
+            //if (manaCheckRof-- > 0)
+            //{
+            //    return;
+            //}
+            //manaCheckRof = 15;
+            //RefreshBar(owner);
         }
 
         public void OnRender(TechnoExt owner)
@@ -74,21 +104,21 @@ namespace Extension.Shared
 
         private void RefreshBar(TechnoExt owner)
         {
-            if (currentMana >= 50)
-            {
-                var ptarget = owner.OwnerObject.Ref;
-                Pointer<WarheadTypeClass> warhead;
-                if (currentMana < 100)
-                {
-                    warhead = pWH1;
-                }
-                else
-                {
-                    warhead = pWH2;
-                }
-                Pointer<BulletClass> pBullet = pBulletType.Ref.CreateBullet(owner.OwnerObject.Convert<AbstractClass>(), owner.OwnerObject, 1, warhead, 100, false);
-                pBullet.Ref.DetonateAndUnInit(ptarget.Base.Base.GetCoords());
-            }
+            //if (currentMana >= 50)
+            //{
+            //    var ptarget = owner.OwnerObject.Ref;
+            //    Pointer<WarheadTypeClass> warhead;
+            //    if (currentMana < 100)
+            //    {
+            //        warhead = pWH1;
+            //    }
+            //    else
+            //    {
+            //        warhead = pWH2;
+            //    }
+            //    Pointer<BulletClass> pBullet = pBulletType.Ref.CreateBullet(owner.OwnerObject.Convert<AbstractClass>(), owner.OwnerObject, 1, warhead, 100, false);
+            //    pBullet.Ref.DetonateAndUnInit(ptarget.Base.Base.GetCoords());
+            //}
 
         }
 
@@ -119,9 +149,9 @@ namespace Extension.Shared
             //    warhead = pWH5;
             //}
 
-            Pointer<BulletClass> pBullet = pBulletType.Ref.CreateBullet(owner.OwnerObject.Convert<AbstractClass>(), owner.OwnerObject, 1, pWH, 100, false);
+            //Pointer<BulletClass> pBullet = pBulletType.Ref.CreateBullet(owner.OwnerObject.Convert<AbstractClass>(), owner.OwnerObject, 1, pWH, 100, false);
 
-            pBullet.Ref.DetonateAndUnInit(ptarget.Base.Base.GetCoords());
+            //pBullet.Ref.DetonateAndUnInit(ptarget.Base.Base.GetCoords());
         }
 
         //, RectangleStruct pBound
@@ -192,5 +222,51 @@ namespace Extension.Shared
             get { return currentMana; }
         }
 
+
+        //private void CreateIdleAnim()
+        //{
+        //    if (!pAnim.IsNull)
+        //    {
+        //        // Logger.Log($"{Game.CurrentFrame} AE[{AEData.Name}]持续动画[{Data.IdleAnim}]已存在，清除再重新创建");
+        //        KillIdleAnim();
+        //    }
+        //    if (string.IsNullOrEmpty(Data.IdleAnim) || pOwner.IsInvisible() || (Data.RemoveInCloak && OnwerIsCloakable))
+        //    {
+        //        return;
+        //    }
+        //    // 创建动画
+        //    // Logger.Log($"{Game.CurrentFrame} AE[{AEData.Name}]创建持续动画[{Data.IdleAnim}]");
+        //    Pointer<AnimTypeClass> pAnimType = AnimTypeClass.ABSTRACTTYPE_ARRAY.Find(Data.IdleAnim);
+        //    if (!pAnimType.IsNull)
+        //    {
+        //        Pointer<AnimClass> pAnim = YRMemory.Create<AnimClass>(pAnimType, pOwner.Ref.Base.GetCoords());
+        //        // Logger.Log($"{Game.CurrentFrame} AE[{AEData.Name}]成功创建持续动画[{Data.IdleAnim}], 指针 {pAnim}");
+        //        // pAnim.Ref.SetOwnerObject(pObject); // 当单位隐形时，附着在单位上的动画会被游戏注销
+        //        // Logger.Log(" - 将动画{0}赋予对象", Data.IdleAnim);
+        //        pAnim.Ref.Loops = 0xFF;
+        //        // Logger.Log(" - 设置动画{0}的剩余迭代次数为{1}", Data.IdleAnim, 0xFF);
+        //        pAnim.SetAnimOwner(pOwner);
+        //        pAnim.Show(Data.Visibility);
+        //        this.pAnim.Pointer = pAnim;
+        //        this.animFlags = pAnim.Ref.AnimFlags;
+        //        // Logger.Log(" - 缓存动画{0}的实例对象指针", Data.IdleAnim);
+        //    }
+        //}
+
+        //private void KillIdleAnim()
+        //{
+        //    if (!pAnim.IsNull)
+        //    {
+        //        // 不将动画附着于单位上，动画就不会自行注销，需要手动注销
+        //        pAnim.Ref.TimeToDie = true;
+        //        pAnim.Ref.Base.UnInit(); // 包含了SetOwnerObject(0) 0x4255B0
+        //        // Logger.Log("{0} - 已销毁动画{1}实例", Game.CurrentFrame, Data.IdleAnim);
+        //        pAnim.Pointer = IntPtr.Zero;
+        //        // Logger.Log("{0} - 成功移除持续动画{1}", Game.CurrentFrame, Data.IdleAnim);
+        //    }
+        //}
+
     }
+
+    //public ManaBarScript:TechnoScriptable
 }
