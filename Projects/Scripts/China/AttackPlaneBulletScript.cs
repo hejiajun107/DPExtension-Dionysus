e2110487cd1cada0d2197d73475ea5d14645acb3
hejiajun107 <hejiajun107@126.com>
2023-01-21 14:42:58 +0800

    歼轰机更新

--- Projects/Scripts/China/AttackPlaneBulletScript.cs
+++ Projects/Scripts/China/AttackPlaneBulletScript.cs
@@ -1,10 +1,96 @@
 ﻿using Extension.Ext;
 using Extension.Script;
+using Extension.Utilities;
 using PatcherYRpp;
 using System;
+using System.Collections.Generic;
 
 namespace DpLib.Scripts.China
 {
+    [Serializable]
+    [ScriptAlias(nameof(AttackPlaneScript))]
+    public class AttackPlaneScript : TechnoScriptable
+    {
+        public AttackPlaneScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        private int currentIndex = 0;
+
+        private int Duration = 0;
+
+        private int damage = 10;
+
+        static Pointer<BulletTypeClass> bullet => BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("JH7Missile");
+        static Pointer<WarheadTypeClass> scanWarhead => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("A5ScanWh");
+
+        static Pointer<AnimTypeClass> FireAnim => AnimTypeClass.ABSTRACTTYPE_ARRAY.Find("A5Fire");
+
+        private Random rd = new Random(114514);
+
+        private CoordStruct targetCoord;
+
+        private List<CoordStruct> FLHS = new List<CoordStruct>()
+        {
+            new CoordStruct(-30,110,-45),
+            new CoordStruct(-30,-110,-45),
+            new CoordStruct(-50,167,-50),
+            new CoordStruct(-50,-167,-50),
+            new CoordStruct(-100,220,-55),
+            new CoordStruct(-100,-220,-55),
+            new CoordStruct(80,0,-80),
+        };
+
+        public CoordStruct GetNextFLH()
+        {
+            if (currentIndex > FLHS.Count - 1)
+            {
+                currentIndex = 0;
+            }
+            var flh = FLHS[currentIndex];
+            currentIndex++;
+            return ExHelper.GetFLHAbsoluteCoords(Owner.OwnerObject, flh, false, 1);
+        }
+
+        public override void OnUpdate()
+        {
+            if (Duration > 0)
+            {
+                if (targetCoord != null)
+                {
+                    var fireStart = GetNextFLH();
+                    var fireEnd = targetCoord + new CoordStruct(rd.Next(-600, 600), rd.Next(-600, 600), 0);
+
+                    if (MapClass.Instance.TryGetCellAt(fireEnd, out var pcell))
+                    {
+                        Pointer<BulletClass> pBullet = bullet.Ref.CreateBullet(pcell.Convert<AbstractClass>(), Owner.OwnerObject, damage, scanWarhead, 100, true);
+                        pBullet.Ref.MoveTo(fireStart, new BulletVelocity(rd.Next(-30, 30), rd.Next(-30, 30), 0));
+                        pBullet.Ref.SetTarget(pcell.Convert<AbstractClass>());
+                        YRMemory.Create<AnimClass>(FireAnim, fireStart);
+                    }
+                }
+
+                if (damage < 25)
+                {
+                    damage++;
+                }
+                Duration--;
+            }
+        }
+
+
+        public override void OnFire(Pointer<AbstractClass> pTarget, int weaponIndex)
+        {
+            if(Duration<=0)
+            {
+                Duration = 30;
+                targetCoord = pTarget.Ref.GetCoords() ;
+            }
+            //base.OnFire(pTarget, weaponIndex);
+        }
+
+    }
+
     [Serializable]
     [ScriptAlias(nameof(AttackPlaneBulletScript))]
     public class AttackPlaneBulletScript : BulletScriptable
@@ -13,7 +99,7 @@ namespace DpLib.Scripts.China
 
         public bool isActived = false;
 
-        TechnoExt pTargetRef;
+        TechnoExt pOwnerExt;
 
         private static ColorStruct innerColor = new ColorStruct(200, 200, 160);
         private static ColorStruct outerColor = new ColorStruct(0, 0, 0);
@@ -21,50 +107,49 @@ namespace DpLib.Scripts.China
 
         private int damage = 8;
 
-        private CoordStruct StartOwner;
-
-        private CoordStruct StartFire;
-
-        static Pointer<BulletTypeClass> bullet => BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
+        static Pointer<BulletTypeClass> bullet => BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("JH7Missile");
         static Pointer<WarheadTypeClass> scanWarhead => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("A5ScanWh");
-        static Pointer<WarheadTypeClass> animWarhead => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("A5SAnimWh");
+
+        static Pointer<AnimTypeClass> FireAnim => AnimTypeClass.ABSTRACTTYPE_ARRAY.Find("A5Fire");
 
         public override void OnUpdate()
         {
             if (isActived == false)
             {
-                pTargetRef = TechnoExt.ExtMap.Find(Owner.OwnerObject.Ref.Owner);
+                pOwnerExt = TechnoExt.ExtMap.Find(Owner.OwnerObject.Ref.Owner);
 
-                if (!pTargetRef.IsNullOrExpired())
+                if (!pOwnerExt.IsNullOrExpired())
                 {
-                    //单位起始位置
-                    StartOwner = pTargetRef.OwnerObject.Ref.Base.Base.GetCoords();
-                    //起始开火点
-                    StartFire = Owner.OwnerObject.Ref.Base.Base.GetCoords();
                     isActived = true;
                 }
             }
 
             if (isActived)
             {
-                if (!pTargetRef.IsNullOrExpired())
+                if (!pOwnerExt.IsNullOrExpired())
                 {
-                    var moveLocation = pTargetRef.OwnerObject.Ref.Base.Base.GetCoords() - StartOwner;
-                    var fireStart = StartFire + moveLocation;
-                    var height = Owner.OwnerObject.Ref.Base.GetHeight();
-                    var fireEnd = Owner.OwnerObject.Ref.Base.Base.GetCoords() - new CoordStruct(0, 0, height);
-                    Pointer<LaserDrawClass> pLaser = YRMemory.Create<LaserDrawClass>(fireStart, fireEnd, innerColor, outerColor, outerSpread, 2);
-
-                    Pointer<BulletClass> pBullet = bullet.Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), pTargetRef.OwnerObject, damage, scanWarhead, 100, true);
-                    pBullet.Ref.DetonateAndUnInit(fireEnd);
-
-                    Pointer<BulletClass> pBulletAnim = bullet.Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), pTargetRef.OwnerObject, 1, animWarhead, 100, false);
-                    pBulletAnim.Ref.DetonateAndUnInit(fireStart);
-
-                    if (damage < 18)
+                    var script = pOwnerExt.GameObject.GetComponent<AttackPlaneScript>();
+                    if (script != null)
                     {
-                        damage++;
+                        var fireStart = script.GetNextFLH();
+                        var height = Owner.OwnerObject.Ref.Base.GetHeight();
+                        var fireEnd = Owner.OwnerObject.Ref.Base.Base.GetCoords() - new CoordStruct(0, 0, height);
+
+                        if(MapClass.Instance.TryGetCellAt(fireEnd,out var pcell))
+                        {
+                            Pointer<BulletClass> pBullet = bullet.Ref.CreateBullet(pcell.Convert<AbstractClass>(), pOwnerExt.OwnerObject, damage, scanWarhead, 100, true);
+                            pBullet.Ref.MoveTo(fireStart, new BulletVelocity());
+                            pBullet.Ref.SetTarget(pcell.Convert<AbstractClass>());
+                            YRMemory.Create<AnimClass>(FireAnim, fireStart);
+                        }
+
+                        if (damage < 18)
+                        {
+                            damage++;
+                        }
+
                     }
+
 
                 }
             }
