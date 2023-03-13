5abc64106e93016f42f6a96b3c27e291e44b3ccc
hejiajun107 <hejiajun107@126.com>
2023-03-13 21:18:48 +0800

    修复纳米视界存档异常

--- Projects/Scripts/AE/NanoSightAttachEffectScript.cs
+++ Projects/Scripts/AE/NanoSightAttachEffectScript.cs
@@ -15,11 +15,14 @@ namespace Scripts.AE
 
         private static Pointer<SuperWeaponTypeClass> revealSW => SuperWeaponTypeClass.ABSTRACTTYPE_ARRAY.Find("PsychicReveal4Jp");
 
-        Pointer<HouseClass> house;
+        HouseExt houseExt;
 
         public override void OnAttachEffectPut(Pointer<int> pDamage, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, Pointer<HouseClass> pAttackingHouse)
         {
-            house = pAttackingHouse;
+            if(!pAttackingHouse.IsNull)
+            {
+                houseExt = HouseExt.ExtMap.Find(pAttackingHouse);
+            }
         }
 
         public override void OnUpdate()
@@ -35,13 +38,13 @@ namespace Scripts.AE
 
         private void RevealAt(CoordStruct location)
         {
-            if (house.IsNull)
+            if (houseExt.IsNullOrExpired())
             {
                 Duration = 0;
                 return;
             }
 
-            Pointer<SuperClass> pSuper = house.Ref.FindSuperWeapon(revealSW);
+            Pointer<SuperClass> pSuper = houseExt.OwnerObject.Ref.FindSuperWeapon(revealSW);
             CellStruct targetCell = CellClass.Coord2Cell(location);
             pSuper.Ref.IsCharged = true;
             pSuper.Ref.Launch(targetCell, true);
