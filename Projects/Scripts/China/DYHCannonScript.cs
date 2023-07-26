7f5c100b05a80491f94ca115fc7d310b3fa9e45f
hejiajun107 <hejiajun107@126.com>
2023-07-26 22:29:05 +0800

    地狱火加农炮mk2

---
+++ Projects/Scripts/China/DYHCannonScript.cs
@@ -0,0 +1,72 @@
+﻿using Extension.Ext;
+using Extension.Script;
+using PatcherYRpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.China
+{
+    [ScriptAlias(nameof(DYHCannonScript))]
+    [Serializable]
+    public class DYHCannonScript : TechnoScriptable
+    {
+        public DYHCannonScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        //贴上mk2的buff
+        static Pointer<WarheadTypeClass> mk2Warhead => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("MarkIIAttachWh");
+
+        static Pointer<WarheadTypeClass> buffWarhead => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("DYHBuffWh");
+
+        static Pointer<BulletTypeClass> pBulletType => BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
+
+        private bool IsMkIIUpdated = false;
+
+        private int Delay = 1200;
+
+        public override void OnUpdate()
+        {
+            if(!IsMkIIUpdated) return;
+
+            if (Delay > 0)
+                Delay--;
+
+            Owner.OwnerObject.Ref.Ammo = Delay <= 0 ? 1 : 0;
+
+            base.OnUpdate();
+        }
+
+        public override void OnFire(Pointer<AbstractClass> pTarget, int weaponIndex)
+        {
+            if (!IsMkIIUpdated)
+                return;
+            if (Delay <= 0)
+            {
+                var pInviso = pBulletType.Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), Owner.OwnerObject, 80, buffWarhead, 100, true);
+                pInviso.Ref.DetonateAndUnInit(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+            }
+
+            Delay = 1200;
+        }
+
+        public override void OnReceiveDamage(Pointer<int> pDamage, int DistanceFromEpicenter, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, bool IgnoreDefenses, bool PreventPassengerEscape, Pointer<HouseClass> pAttackingHouse)
+        {
+            if (IsMkIIUpdated == false)
+            {
+                //判断是否来自升级弹头
+                if (pWH.Ref.Base.ID.ToString() == "MarkIISpWh")
+                {
+                    IsMkIIUpdated = true;
+                    Pointer<TechnoClass> pTechno = Owner.OwnerObject;
+                    CoordStruct currentLocation = pTechno.Ref.Base.Base.GetCoords();
+                    Pointer<BulletClass> mk2bullet = pBulletType.Ref.CreateBullet(pTechno.Convert<AbstractClass>(), Owner.OwnerObject, 1, mk2Warhead, 100, false);
+                    mk2bullet.Ref.DetonateAndUnInit(currentLocation);
+                }
+            }
+        }
+    }
+}
