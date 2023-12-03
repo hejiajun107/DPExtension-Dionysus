b8cd366e0f86c7aac01b142f99bf53041ffc13b1
hejiajun107 <hejiajun107@126.com>
2023-12-03 13:33:54 +0800

    尤里T3塔

---
+++ Projects/Scripts/Soviet/STankBunkerScript.cs
@@ -0,0 +1,76 @@
+﻿using DynamicPatcher;
+using Extension.Ext;
+using Extension.Script;
+using PatcherYRpp;
+using PatcherYRpp.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Soviet
+{
+    [ScriptAlias(nameof(STankBunkerScript))]
+    [Serializable]
+    public class STankBunkerScript : TechnoScriptable
+    {
+        public STankBunkerScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        private int rof = 100;
+
+        private static List<CoordStruct> coordOffets = new List<CoordStruct>()
+        {
+            new CoordStruct(256,256,30),
+            new CoordStruct(256,-256,30),
+            new CoordStruct(-256,256,30),
+            new CoordStruct(-256,-256,30),
+        };
+
+        private int currentChargeIndex = 0;
+
+        private static Pointer<WeaponTypeClass> chargeWeapon => WeaponTypeClass.ABSTRACTTYPE_ARRAY.Find("AssaultBolt");
+
+        public override void OnUpdate()
+        {
+            if (rof-- > 0)
+                return;
+
+            rof = 100;
+
+            if (Owner.OwnerObject.Ref.BunkerLinkedItem.IsNull)
+                return;
+
+            var strength = Owner.OwnerObject.Ref.BunkerLinkedItem.Ref.Type.Ref.Base.Strength;
+            if(Owner.OwnerObject.Ref.BunkerLinkedItem.Ref.Base.Health < strength)
+            {
+                Owner.OwnerObject.Ref.BunkerLinkedItem.Ref.Base.Health = Owner.OwnerObject.Ref.BunkerLinkedItem.Ref.Base.Health + 5 > strength ? strength : Owner.OwnerObject.Ref.BunkerLinkedItem.Ref.Base.Health + 5;
+            }
+
+            Owner.OwnerObject.Ref.Electric_Zap(Owner.OwnerObject.Ref.BunkerLinkedItem.Convert<AbstractClass>(), chargeWeapon, Owner.OwnerObject.Ref.Base.Base.GetCoords() + coordOffets[currentChargeIndex]);
+
+            currentChargeIndex++;
+            if (currentChargeIndex > 3)
+                currentChargeIndex = 0;
+
+            var pInviso = BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
+            var pBullet = pInviso.Ref.CreateBullet(Owner.OwnerObject.Ref.BunkerLinkedItem.Convert<AbstractClass>(), Owner.OwnerObject, 0, WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("TKBEffectWh"), 100, true);
+            pBullet.Ref.DetonateAndUnInit(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+        }
+
+        public override void OnReceiveDamage(Pointer<int> pDamage, int DistanceFromEpicenter, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, bool IgnoreDefenses, bool PreventPassengerEscape, Pointer<HouseClass> pAttackingHouse)
+        {
+            int trueDamage = MapClass.GetTotalDamage(pDamage.Ref, pWH, Owner.OwnerObject.Ref.Type.Ref.Base.Armor, DistanceFromEpicenter);
+
+            if (trueDamage > 1)
+            {
+                if (DistanceFromEpicenter > Game.CellSize)
+                {
+                    pDamage.Ref =(int)(pDamage.Ref * 0.2);
+                }
+            }
+        }
+    }
+}
---
+++ Projects/Scripts/Yuri/PlastTowerScript.cs
@@ -0,0 +1,126 @@
+﻿using Extension.Ext;
+using Extension.Script;
+using PatcherYRpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Yuri
+{
+    [ScriptAlias(nameof(PlastTowerScript))]
+    [Serializable]
+    public class PlastTowerScript : TechnoScriptable
+    {
+        public PlastTowerScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        private int level = 0;
+
+        public int clearDelay = 100;
+
+        private static Pointer<WarheadTypeClass> pWh => WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("PlastWaveWh");
+
+        public override void OnUpdate()
+        {
+            if(clearDelay--<=0)
+            {
+                level = 0;
+            }
+        }
+
+        public override void OnFire(Pointer<AbstractClass> pTarget, int weaponIndex)
+        {
+            if(pTarget.CastToTechno(out var ptechno))
+            {
+                if(ptechno.Ref.Owner.Ref.IsAlliedWith(Owner.OwnerObject.Ref.Owner))
+                {
+                    var mission = Owner.OwnerObject.Convert<MissionClass>();
+                    mission.Ref.ForceMission(Mission.Stop);
+                    return;
+                }
+            }
+            else
+            {
+                var mission = Owner.OwnerObject.Convert<MissionClass>();
+                mission.Ref.ForceMission(Mission.Stop);
+                return;
+            }
+         
+            clearDelay = 100;
+            var pInviso = BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
+            var pBullet = pInviso.Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), Owner.OwnerObject, 5 + level, pWh, 100, false);
+            pBullet.Ref.DetonateAndUnInit(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+            level++;
+            if (level > 30)
+                level = 30;
+        }
+    }
+
+    [ScriptAlias(nameof(PlastTowerAttachEffect))]
+    [Serializable]
+    public class PlastTowerAttachEffect : AttachEffectScriptable
+    {
+        public PlastTowerAttachEffect(TechnoExt owner) : base(owner)
+        {
+        }
+
+        TechnoExt attacker;
+
+        private static Pointer<AnimTypeClass> pTowerAnim => AnimTypeClass.ABSTRACTTYPE_ARRAY.Find("EnergyPlst");
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+
+        public override void OnFire(Pointer<AbstractClass> pTarget, int weaponIndex)
+        {
+            if (attacker.IsNullOrExpired())
+                return;
+
+            if (Owner.OwnerObject.Ref.Type.Ref.ImmuneToPsionics)
+                return;
+
+            if (attacker.OwnerObject.Ref.Owner.Ref.IsAlliedWith(Owner.OwnerObject.Ref.Owner))
+                return;
+
+            if (Owner.OwnerObject.Ref.Base.InLimbo)
+                return;
+
+            YRMemory.Create<AnimClass>(pTowerAnim, attacker.OwnerRef.Base.Base.GetCoords() + new CoordStruct(0, 0, 888));
+
+            var damage = (int)(Owner.OwnerObject.Ref.Type.Ref.Base.Strength / 10);
+
+            var inviso = BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
+            var pBullet = inviso.Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), attacker.OwnerObject, damage, WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("PlastFBWh"), 100, true);
+            pBullet.Ref.DetonateAndUnInit(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+        }
+
+        public override void OnAttachEffectPut(Pointer<int> pDamage, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, Pointer<HouseClass> pAttackingHouse)
+        {
+            if (pAttacker.IsNotNull)
+            {
+                if (pAttacker.CastToTechno(out var ptechno))
+                {
+                    attacker = TechnoExt.ExtMap.Find(ptechno);
+                }
+            }
+        }
+
+        public override void OnAttachEffectRecieveNew(int duration, Pointer<int> pDamage, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, Pointer<HouseClass> pAttackingHouse)
+        {
+            Duration = duration;
+
+            if(pAttacker.IsNotNull)
+            {
+                if(pAttacker.CastToTechno(out var ptechno))
+                {
+                    attacker = TechnoExt.ExtMap.Find(ptechno);
+                }
+            }
+        }
+    }
+}
