2b817e070b184ae909793b543d06cf3d960c17fa
hejiajun107 <hejiajun107@126.com>
2023-12-04 20:22:18 +0800

    坦克碉堡

--- Projects/Scripts/Soviet/STankBunkerScript.cs
+++ Projects/Scripts/Soviet/STankBunkerScript.cs
@@ -20,6 +20,7 @@ namespace Scripts.Soviet
         }
 
         private int rof = 100;
+        private int erof = 30;
 
         private static List<CoordStruct> coordOffets = new List<CoordStruct>()
         {
@@ -30,19 +31,44 @@ namespace Scripts.Soviet
         };
 
         private int currentChargeIndex = 0;
+        private int currentEIndex = 0;
 
         private static Pointer<WeaponTypeClass> chargeWeapon => WeaponTypeClass.ABSTRACTTYPE_ARRAY.Find("AssaultBolt");
 
         public override void OnUpdate()
         {
-            if (rof-- > 0)
+            if(!CanWork())
+            {
                 return;
-
-            rof = 100;
+            }
 
             if (Owner.OwnerObject.Ref.BunkerLinkedItem.IsNull)
                 return;
 
+            if (erof <= 0)
+            {
+                erof = 30;
+                Owner.OwnerObject.Ref.Electric_Zap(Owner.OwnerObject.Convert<AbstractClass>(), chargeWeapon, Owner.OwnerObject.Ref.Base.Base.GetCoords() + coordOffets[currentChargeIndex]);
+                Pointer<EBolt> pBolt = YRMemory.Create<EBolt>();
+                if (!pBolt.IsNull)
+                {
+                    var eSource = Owner.OwnerObject.Ref.Base.Base.GetCoords() + coordOffets[currentEIndex];
+                    pBolt.Ref.Fire(eSource, eSource + new CoordStruct(MathEx.Random.Next(-50, 50), MathEx.Random.Next(-50, 50), 100), 0);
+                    pBolt.Ref.AlternateColor = false;
+                    currentEIndex++;
+                    if (currentEIndex > 3)
+                        currentEIndex = 0;
+                }
+            }
+            else
+            {
+                erof--;
+            }
+
+            if (rof-- > 0)
+                return;
+
+            rof = 100;
             var strength = Owner.OwnerObject.Ref.BunkerLinkedItem.Ref.Type.Ref.Base.Strength;
             if(Owner.OwnerObject.Ref.BunkerLinkedItem.Ref.Base.Health < strength)
             {
@@ -58,6 +84,12 @@ namespace Scripts.Soviet
             var pInviso = BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
             var pBullet = pInviso.Ref.CreateBullet(Owner.OwnerObject.Ref.BunkerLinkedItem.Convert<AbstractClass>(), Owner.OwnerObject, 0, WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("TKBEffectWh"), 100, true);
             pBullet.Ref.DetonateAndUnInit(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+        }
+
+        public bool CanWork()
+        {
+            double powerP = Owner.OwnerObject.Ref.Owner.Ref.GetPowerPercentage();
+            return Owner.OwnerObject.Ref.IsPowerOnline() & (powerP >= 1);
         }
 
         public override void OnReceiveDamage(Pointer<int> pDamage, int DistanceFromEpicenter, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, bool IgnoreDefenses, bool PreventPassengerEscape, Pointer<HouseClass> pAttackingHouse)
