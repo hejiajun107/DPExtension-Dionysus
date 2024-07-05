581bcbceb610dfc98d2e98f2b26b290fc9130062
hejiajun107 <hejiajun107@126.com>
2024-07-05 21:22:06 +0800

    各种装备

--- Projects/Scripts/Heros/ColdBladeScript.cs
+++ Projects/Scripts/Heros/ColdBladeScript.cs
@@ -2,7 +2,9 @@
 using Extension.Script;
 using Extension.Shared;
 using PatcherYRpp;
+using PatcherYRpp.Utilities;
 using System;
+using System.Linq;
 
 namespace DpLib.Scripts.Heros
 {
@@ -118,4 +120,205 @@ namespace DpLib.Scripts.Heros
 
         }
     }
+
+
+    [Serializable]
+    [ScriptAlias(nameof(TeslaAmmoAttachEffectScript))]
+    public class TeslaAmmoAttachEffectScript : AttachEffectScriptable
+    {
+        public TeslaAmmoAttachEffectScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+
+        public override void OnAttachEffectPut(Pointer<int> pDamage, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, Pointer<HouseClass> pAttackingHouse)
+        {
+           
+        }
+
+        public override void OnUpdate()
+        {
+            Duration = 100;
+        }
+
+        public override void OnFire(Pointer<AbstractClass> pTarget, int weaponIndex)
+        {
+            if(weaponIndex == 0)
+            {
+                var location = pTarget.Ref.GetCoords();
+                var technos = ObjectFinder.FindTechnosNear(location, 5 * Game.CellSize)
+                    .OrderByDescending(x => x.Ref.Base.GetCoords()
+                    .DistanceFrom(location))
+                    .Select(x => x.Convert<TechnoClass>())
+                    .Where(x => !x.Ref.Owner.Ref.IsAlliedWith(Owner.OwnerObject.Ref.Owner)).Take(3)
+                    .ToList();
+
+                var count = technos.Count();
+
+                CoordStruct last = pTarget.Ref.GetCoords();
+                
+                var warhead = WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("Electric");
+                var warheadM = WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("TankElectric");
+                var bullet = BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
+
+                var pfBullet = bullet.Ref.CreateBullet(pTarget.Convert<AbstractClass>(), Owner.OwnerObject, 20, warheadM, 100, true);
+                pfBullet.Ref.DetonateAndUnInit(pTarget.Ref.GetCoords());
+
+                for (var i = 0; i < count; i++)
+                {
+                    var techno = technos[i];
+                    if(techno.Ref.Base.Base.GetCoords().DistanceFrom(pTarget.Ref.GetCoords())<100)
+                    {
+                        continue;
+                    }
+                    var pBullet = bullet.Ref.CreateBullet(techno.Convert<AbstractClass>(), Owner.OwnerObject, 30, warhead, 100, true);
+                    pBullet.Ref.DetonateAndUnInit(techno.Ref.Base.Base.GetCoords());
+                    if (i > 0)
+                    {
+                        Pointer<EBolt> pBolt = YRMemory.Create<EBolt>();
+                        if (!pBolt.IsNull)
+                        {
+                            pBolt.Ref.Fire(last, techno.Ref.Base.Base.GetCoords(), 0);
+                            pBolt.Ref.AlternateColor = false;
+                        }
+                        last = techno.Ref.Base.Base.GetCoords();
+                    }
+                }
+
+                if (count < 3)
+                {
+                    for (var j = 0; j < 3 - count; j++)
+                    {
+                        var target = location + new CoordStruct(MathEx.Random.Next(-5 * Game.CellSize, 5 * Game.CellSize), MathEx.Random.Next(-5 * Game.CellSize, 5 * Game.CellSize), 0);
+                        var pBullet = bullet.Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), Owner.OwnerObject, 30, warhead, 100, true);
+                        pBullet.Ref.DetonateAndUnInit(target);
+
+                        Pointer<EBolt> pBolt = YRMemory.Create<EBolt>();
+                        if (!pBolt.IsNull)
+                        {
+                            pBolt.Ref.Fire(last, target, 0);
+                            pBolt.Ref.AlternateColor = false;
+                        }
+                        last = target;
+                    }
+                }
+
+
+            }
+        }
+
+
+
+
+    }
+
+
+
+    [Serializable]
+    [ScriptAlias(nameof(LaserPointerAttachEffectScript))]
+    public class LaserPointerAttachEffectScript : AttachEffectScriptable
+    {
+        public LaserPointerAttachEffectScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        private int rof = 80;
+
+
+        public override void OnAttachEffectPut(Pointer<int> pDamage, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, Pointer<HouseClass> pAttackingHouse)
+        {
+
+        }
+
+        public override void OnUpdate()
+        {
+            Duration = 100;
+            if(rof>0)
+            {
+                rof--;
+            }
+        }
+
+        public override void OnFire(Pointer<AbstractClass> pTarget, int weaponIndex)
+        {
+            if(weaponIndex==0 && rof <= 0)
+            {
+                rof = 80;
+
+                var laserColor = new ColorStruct(255, 0, 0);
+                Pointer<LaserDrawClass> pLaser = YRMemory.Create<LaserDrawClass>(Owner.OwnerObject.Ref.Base.Base.GetCoords() + new CoordStruct(0,0,100), pTarget.Ref.GetCoords(), laserColor, laserColor, laserColor, 10);
+                pLaser.Ref.Thickness = 5;
+                pLaser.Ref.IsHouseColor = true;
+
+                var bullet = BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
+                var pBullet = bullet.Ref.CreateBullet(pTarget, Owner.OwnerObject, 1,WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("LaserPointerWh") , 100, true);
+                pBullet.Ref.DetonateAndUnInit(pTarget.Ref.GetCoords());
+            }
+        }
+
+    }
+
+    [Serializable]
+    [ScriptAlias(nameof(MedicalPackageAttchEffectScript))]
+    public class MedicalPackageAttchEffectScript : AttachEffectScriptable
+    {
+        public MedicalPackageAttchEffectScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        private int delay = 500;
+        private int rof = 30;
+
+        public override void OnUpdate()
+        {
+            Duration = 100;
+
+            if (Owner.OwnerObject.Ref.Base.InLimbo)
+                return;
+
+            if (delay > 0)
+            {
+                delay--;
+            }
+
+            if(delay>0)
+                return;
+
+            if (rof > 0)
+            {
+                rof--;
+                return;
+            }
+
+            var location = Owner.OwnerObject.Ref.Base.Base.GetCoords();
+            var techno = ObjectFinder.FindTechnosNear(location, 3 * Game.CellSize).Where(x => x.Ref.Base.WhatAmI() == AbstractType.Infantry)
+                .Select(x => x.Convert<TechnoClass>()).
+                Where(x => x.Ref.Owner.Ref.IsAlliedWith(Owner.OwnerObject.Ref.Owner))
+                .Where(x => x.Ref.Base.Health < x.Ref.Type.Ref.Base.Strength).OrderBy(x => x.Ref.Base.Base.GetCoords().DistanceFrom(location))
+                .FirstOrDefault();
+                ;
+
+            if(techno != null)
+            {
+                rof = 30;
+                var health = techno.Ref.Base.Health + 30;
+                techno.Ref.Base.Health = techno.Ref.Type.Ref.Base.Strength > health ? health : techno.Ref.Type.Ref.Base.Strength;
+                YRMemory.Create<AnimClass>(AnimTypeClass.ABSTRACTTYPE_ARRAY.Find("VOLHEAL"), techno.Ref.Base.Base.GetCoords());
+            }
+
+        }
+
+        public override void OnFire(Pointer<AbstractClass> pTarget, int weaponIndex)
+        {
+            delay = 500;
+            base.OnFire(pTarget, weaponIndex);
+        }
+
+        public override void OnReceiveDamage(Pointer<int> pDamage, int DistanceFromEpicenter, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, bool IgnoreDefenses, bool PreventPassengerEscape, Pointer<HouseClass> pAttackingHouse)
+        {
+            delay = 500;
+            base.OnReceiveDamage(pDamage, DistanceFromEpicenter, pWH, pAttacker, IgnoreDefenses, PreventPassengerEscape, pAttackingHouse);
+        }
+    }
+
 }
---
+++ Projects/Scripts/SpecialCrateScript.cs
@@ -0,0 +1,84 @@
+﻿using Extension.Ext;
+using Extension.INI;
+using Extension.Script;
+using Newtonsoft.Json;
+using PatcherYRpp;
+using PatcherYRpp.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts
+{
+    [ScriptAlias(nameof(SpecialCrateScript))]
+    [Serializable]
+    public class SpecialCrateScript : TechnoScriptable
+    {
+        public SpecialCrateScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        private int rof = 5;
+        private string warhead = string.Empty;
+        private List<string> required = new List<string>();
+
+        public override void Awake()
+        {
+            var ini = this.CreateRulesIniComponentWith<SpecialCrateData>(Owner.OwnerObject.Ref.Type.Ref.Base.Base.ID);
+            warhead = ini.Data.Warhead;
+
+            if(!string.IsNullOrWhiteSpace(ini.Data.Required))
+            {
+                required = ini.Data.Required.Split(',').ToList();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (rof-- > 0)
+                return;
+
+            rof = 5;
+
+            var location = Owner.OwnerObject.Ref.Base.Base.GetCoords();
+
+            var technos = ObjectFinder.FindTechnosNear(location, 1 * Game.CellSize).OrderByDescending(x=>x.Ref.Base.GetCoords().DistanceFrom(location)).ToList();
+
+            if (!technos.Any())
+                return;
+
+            Pointer<ObjectClass> target;
+
+            if(!required.Any())
+                target = technos.First();
+            else
+                target = technos.Where(x => required.Contains(x.Convert<TechnoClass>().Ref.Type.Ref.Base.Base.ID)).FirstOrDefault();
+
+            if (target == null || target.IsNull)
+                return;
+
+            if (!string.IsNullOrWhiteSpace(warhead))
+            {
+                var pbullet = BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible").Ref.CreateBullet(target.Convert<AbstractClass>(), Owner.OwnerObject, 1, WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find(warhead), 100, false);
+                pbullet.Ref.DetonateAndUnInit(target.Ref.Base.GetCoords());
+            }
+
+            Owner.OwnerObject.Ref.Base.Remove();
+            Owner.OwnerObject.Ref.Base.UnInit();
+
+        }
+    }
+
+    public class SpecialCrateData : INIAutoConfig
+    {
+        [INIField(Key = "SpecialCrate.Required")]
+        public string Required = "";
+
+        [INIField(Key = "SpecialCrate.Warhead")]
+        public string Warhead = "";
+
+    }
+
+}
