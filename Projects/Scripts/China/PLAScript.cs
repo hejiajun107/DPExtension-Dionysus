e457fb9c9c0ba0c8c017f6aec19b8d19aaaf6143
hejiajun107 <hejiajun107@126.com>
2025-03-13 22:39:53 +0800

    思金突袭兵

--- Projects/Scripts/China/PLAScript.cs
+++ Projects/Scripts/China/PLAScript.cs
@@ -1,6 +1,8 @@
 ﻿using Extension.Ext;
 using Extension.Script;
+using Extension.Utilities;
 using PatcherYRpp;
+using PatcherYRpp.Utilities;
 using System;
 
 namespace DpLib.Scripts.China
@@ -17,8 +19,15 @@ namespace DpLib.Scripts.China
         private static Pointer<TechnoTypeClass> targetType => TechnoTypeClass.ABSTRACTTYPE_ARRAY.Find("GATP2");
 
         public bool inited = false;
+		public int AmmoType { get; set; } = 1;
 
-        public override void OnUpdate()
+		public override void Awake()
+		{
+			AmmoType = MathEx.Random.Next(100) > 50 ? 1 : 2;
+		}
+
+
+		public override void OnUpdate()
         {
             if (!inited)
             {
@@ -57,4 +66,202 @@ namespace DpLib.Scripts.China
 
 
     }
+
+	[Serializable]
+	[ScriptAlias(nameof(PLAUltraBulletScript))]
+	public class PLAUltraBulletScript : BulletScriptable
+	{
+		public PLAUltraBulletScript(BulletExt owner) : base(owner)
+		{
+		}
+
+        private bool inited = false;
+
+		public override void OnUpdate()
+		{
+            if (inited) {
+                return;
+            }
+
+            inited = true;
+
+            var ammoType = 0;
+
+            if(Owner.OwnerObject.Ref.Owner.IsNotNull)
+            {
+                var technoExt = TechnoExt.ExtMap.Find(Owner.OwnerObject.Ref.Owner);
+                if(!technoExt.IsNullOrExpired())
+                {
+                    var plaScript = technoExt.GameObject.GetComponent<PLAScript>();
+                    
+					if (plaScript != null)
+                    {
+                        ammoType = plaScript.AmmoType;
+                    }
+                }
+            }
+
+
+            if (ammoType == 1)
+            {
+				Pointer<LaserDrawClass> pLaser = YRMemory.Create<LaserDrawClass>(Owner.OwnerObject.Ref.SourceCoords, Owner.OwnerObject.Ref.TargetCoords, new ColorStruct(255, 0, 0), new ColorStruct(0, 0, 0), new ColorStruct(0, 0, 0), 1);
+				var pb = BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible").Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), Owner.OwnerObject.Ref.Owner, 1, WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("GATPFlameAE"), 100, true);
+				pb.Ref.DetonateAndUnInit(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+			}
+			else if (ammoType == 2)
+            {
+				Pointer<LaserDrawClass> pLaser = YRMemory.Create<LaserDrawClass>(Owner.OwnerObject.Ref.SourceCoords, Owner.OwnerObject.Ref.TargetCoords, new ColorStruct(0, 0, 255), new ColorStruct(0, 0, 0), new ColorStruct(0, 0, 0), 1);
+				var pb = BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible").Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), Owner.OwnerObject.Ref.Owner, 1, WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("GATPBoltAE"), 100, true);
+				pb.Ref.DetonateAndUnInit(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+			}
+			else
+            {
+				Pointer<LaserDrawClass> pLaser = YRMemory.Create<LaserDrawClass>(Owner.OwnerObject.Ref.SourceCoords, Owner.OwnerObject.Ref.TargetCoords, new ColorStruct(200, 200, 160), new ColorStruct(0, 0, 0), new ColorStruct(0, 0, 0), 1);
+			}
+
+
+			base.OnUpdate();
+		}
+	}
+
+
+
+
+	[Serializable]
+	[ScriptAlias(nameof(PlaFlameAttachEffectScript))]
+	public class PlaFlameAttachEffectScript : AttachEffectScriptable
+	{
+	
+		private int count = 1;
+
+		private Pointer<BulletTypeClass> inviso => BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
+
+
+		public PlaFlameAttachEffectScript(TechnoExt owner) : base(owner)
+		{
+		}
+
+		public override void OnUpdate()
+		{
+			
+		}
+
+		public override void OnReceiveDamage(Pointer<int> pDamage, int DistanceFromEpicenter, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, bool IgnoreDefenses, bool PreventPassengerEscape, Pointer<HouseClass> pAttackingHouse)
+		{
+
+		}
+
+		public override void OnAttachEffectPut(Pointer<int> pDamage, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, Pointer<HouseClass> pAttackingHouse)
+		{
+			base.OnAttachEffectPut(pDamage, pWH, pAttacker, pAttackingHouse);
+		}
+
+		//重置duration
+		public override void OnAttachEffectRecieveNew(int duration, Pointer<int> pDamage, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, Pointer<HouseClass> pAttackingHouse)
+		{
+			if (count < 9)
+			{
+				count += 1;
+			}
+			else
+			{
+				if(pAttacker.IsNotNull)
+				{
+					if(pAttacker.CastToTechno(out var pattckTechno))
+					{
+						count = 0;
+						var pb = inviso.Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), pattckTechno, 30, WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("GATPFlameWh"), 100, true);
+						pb.Ref.DetonateAndUnInit(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+					}
+				}
+			}
+
+			Duration = duration;
+		}
+
+		public override void OnRemove()
+		{
+			Duration = 0;
+			base.OnRemove();
+		}
+
+		public override void OnAttachEffectRemove()
+		{
+			base.OnAttachEffectRemove();
+		}
+	}
+
+
+
+
+
+
+
+	[Serializable]
+	[ScriptAlias(nameof(PlaThunderAttachEffectScript))]
+	public class PlaThunderAttachEffectScript : AttachEffectScriptable
+	{
+
+		private int count = 1;
+
+		private Pointer<BulletTypeClass> inviso => BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible");
+
+
+		public PlaThunderAttachEffectScript(TechnoExt owner) : base(owner)
+		{
+		}
+
+		public override void OnUpdate()
+		{
+
+		}
+
+		public override void OnReceiveDamage(Pointer<int> pDamage, int DistanceFromEpicenter, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, bool IgnoreDefenses, bool PreventPassengerEscape, Pointer<HouseClass> pAttackingHouse)
+		{
+
+		}
+
+		public override void OnAttachEffectPut(Pointer<int> pDamage, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, Pointer<HouseClass> pAttackingHouse)
+		{
+			base.OnAttachEffectPut(pDamage, pWH, pAttacker, pAttackingHouse);
+		}
+
+		//重置duration
+		public override void OnAttachEffectRecieveNew(int duration, Pointer<int> pDamage, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, Pointer<HouseClass> pAttackingHouse)
+		{
+			if (count < 9)
+			{
+				count += 1;
+			}
+			else
+			{
+				if (pAttacker.IsNotNull)
+				{
+					if (pAttacker.CastToTechno(out var pattckTechno))
+					{
+						count = 0;
+						var pb = inviso.Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), pattckTechno, 10, WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("GATPBoltWh"), 100, true);
+						pb.Ref.DetonateAndUnInit(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+					}
+				}
+			}
+
+			Duration = duration;
+		}
+
+		public override void OnRemove()
+		{
+			Duration = 0;
+			base.OnRemove();
+		}
+
+		public override void OnAttachEffectRemove()
+		{
+			base.OnAttachEffectRemove();
+		}
+	}
+
+
+
+
 }
---
+++ Projects/Scripts/Scrin/SCVanishScript.cs
@@ -0,0 +1,82 @@
+﻿using Extension.Ext;
+using Extension.Script;
+using PatcherYRpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Scrin
+{
+	[ScriptAlias(nameof(SCVanishScript))]
+	[Serializable]
+	public class SCVanishScript : TechnoScriptable
+	{
+		public SCVanishScript(TechnoExt owner) : base(owner)
+		{
+		}
+
+		public override void OnFire(Pointer<AbstractClass> pTarget, int weaponIndex)
+		{
+			if (weaponIndex != 1)
+			{
+				return;
+			}
+
+			if (pTarget.CastToTechno(out var ptechno))
+			{
+                if (!ptechno.Ref.Owner.Ref.IsAlliedWith(Owner.OwnerObject.Ref.Owner))
+                {
+					return;
+                }
+                TrySetLocation(ptechno, pTarget.Ref.GetCoords());
+			}
+		}
+
+
+		private bool TrySetLocation(Pointer<TechnoClass> techno, CoordStruct location)
+		{
+			if (!Owner.OwnerObject.Ref.Owner.IsNull)
+			{
+				var pTechno = Owner.OwnerObject;
+				var mission = pTechno.Convert<MissionClass>();
+				mission.Ref.ForceMission(Mission.Stop);
+				var currentLocation = Owner.OwnerObject.Ref.Base.Base.GetCoords();
+
+				//= O
+
+				//位置
+				if (pTechno.CastToFoot(out Pointer<FootClass> pfoot))
+				{
+					if (MapClass.Instance.TryGetCellAt(location, out Pointer<CellClass> pCell))
+					{
+						//源动画
+						var source = pTechno.Ref.Base.Base.GetCoords();
+
+						YRMemory.Create<AnimClass>(AnimTypeClass.ABSTRACTTYPE_ARRAY.Find("QGROCX03"), source);
+
+						pfoot.Ref.Locomotor.Mark_All_Occupation_Bits(0);
+						pfoot.Ref.Locomotor.Force_Track(-1, source);
+						pTechno.Ref.Base.UnmarkAllOccupationBits(source);
+						//var cLocal = pCell.Ref.Base.GetCoords();
+						//var pLocal = new CoordStruct(cLocal.X, cLocal.Y, location.Z);
+						pTechno.Ref.Base.SetLocation(location);
+						pTechno.Ref.Base.UnmarkAllOccupationBits(location);
+
+						YRMemory.Create<AnimClass>(AnimTypeClass.ABSTRACTTYPE_ARRAY.Find("QGROCX03"), location);
+
+						pTechno.Ref.Base.Scatter(location, true, true);
+
+						//目标动画
+					}
+				}
+
+				return true;
+			}
+
+
+			return false;
+		}
+	}
+}
