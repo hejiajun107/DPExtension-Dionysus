c7ce36e08ea3b235a364e7ab5c6f0efc623bc472
hejiajun107 <hejiajun107@126.com>
2025-03-20 23:05:33 +0800

    贝顿移动开火

--- Projects/Scripts/Heros/BadunScript.cs
+++ Projects/Scripts/Heros/BadunScript.cs
@@ -1,10 +1,15 @@
 ﻿
+using DynamicPatcher;
+using Extension.EventSystems;
 using Extension.Ext;
 using Extension.Script;
 using Extension.Shared;
 using Extension.Utilities;
 using PatcherYRpp;
+using PatcherYRpp.Utilities;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Scripts
 {
@@ -22,14 +27,7 @@ namespace Scripts
 
         static BadunScript()
         {
-            // Task.Run(() =>
-            // {
-            //     while (true)
-            //     {
-            //         Logger.Log("Ticked.");
-            //         Thread.Sleep(1000);
-            //     }
-            // });
+          
         }
 
         Random random = new Random(110512);
@@ -83,18 +81,19 @@ namespace Scripts
 
             int maxKeep = 350;
 
+
             if (reloaded)
             {
                 if (currentDuration >= maxKeep)
                 {
-                    reloaded = false;
-                    burstRate = 10;
-                    currentDuration = 0;
+                    EndBurst();
                 }
                 else
                 {
                     currentDuration++;
                     var ammo = 6 - (int)((currentDuration / (double)maxKeep) * 6);
+
+
                     if (ammo < 0)
                     {
                         ammo = 0;
@@ -151,6 +150,8 @@ namespace Scripts
         //{
         //    _manaCounter.OnRender(Owner);
         //}
+
+    
         public override void OnFire(Pointer<AbstractClass> pTarget, int weaponIndex)
         {
             bool controlledByAi = false;
@@ -197,10 +198,139 @@ namespace Scripts
                 //var panim = YRMemory.Create<AnimClass>(AnimTypeClass.ABSTRACTTYPE_ARRAY.Find("DYRELOADEDEFFECT"), pTechno.Ref.Base.Base.GetCoords());
                 //panim.Ref.SetOwnerObject(pTechno.Convert<ObjectClass>());
 
+                if (Owner.OwnerObject.Ref.Owner.Ref.ControlledByHuman())
+                {
+                    //Pointer<BulletClass> pBullet = pBulletType.Ref.CreateBullet(pTechno.Convert<AbstractClass>(), Owner.OwnerObject, 1, WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("BDBurstDWWH"), 100, false);
+                    //pBullet.Ref.DetonateAndUnInit(pTechno.Ref.Base.Base.GetCoords());
+                }
+
                 reloaded = true;
                 currentDuration = 0;
                 burstRate = 60;
+
+                EventSystem.General.AddTemporaryHandler(EventSystem.General.LogicClassUpdateEvent, AutoFireUpdate);
             }
         }
+
+
+        private void EndBurst()
+        {
+            reloaded = false;
+            burstRate = 10;
+            currentDuration = 0;
+            EventSystem.General.RemoveTemporaryHandler(EventSystem.General.LogicClassUpdateEvent, AutoFireUpdate);
+        }
+
+
+        //private int delay = 0;
+
+        private TechnoExt currentTarget;
+
+        private int autoFireRof = 0;
+
+        public void AutoFireUpdate(object sender, EventArgs args) 
+        {
+            if(args is LogicClassUpdateEventArgs largs)
+            {
+                if (!largs.IsLateUpdate)
+                    return;
+            }
+            else
+            {
+                return;
+            }
+            if (!Owner.OwnerObject.Ref.Owner.Ref.ControlledByHuman())
+            {
+                return;
+            }
+
+
+            if(Owner.OwnerObject.Ref.Target.IsNotNull)
+            {
+                if(Owner.OwnerObject.Ref.Target.CastToTechno(out var targetTechno))
+                {
+                    if (currentTarget.IsNullOrExpired())
+                    {
+                        currentTarget = TechnoExt.ExtMap.Find(targetTechno);
+                    }
+                    else
+                    {
+                        if(currentTarget.OwnerObject != targetTechno)
+                        {
+                            currentTarget = TechnoExt.ExtMap.Find(targetTechno);
+                        }
+                    }
+                }
+            }
+
+            if (currentTarget.IsNullOrExpired())
+            {
+                var picked = FindTarget(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+                if (picked.IsNotNull)
+                {
+                    currentTarget = TechnoExt.ExtMap.Find(picked.Convert<TechnoClass>());
+                }
+            }
+
+
+
+            if (!currentTarget.IsNullOrExpired())
+            {
+                if (Owner.OwnerObject.Ref.Base.Base.GetCoords().BigDistanceForm(currentTarget.OwnerObject.Ref.Base.Base.GetCoords()) > 7 * Game.CellSize)
+                {
+                    return;
+                }
+
+                if (Owner.OwnerObject.CastToFoot(out Pointer<FootClass> pfoot))
+                {
+                    var dir = GameUtil.Point2Dir(Owner.OwnerObject.Ref.Base.Base.GetCoords(), currentTarget.OwnerObject.Ref.Base.Base.GetCoords());
+                    var tdir = new DirStruct(16, (short)DirStruct.TranslateFixedPoint(3, 16, (uint)dir));
+                    Owner.OwnerObject.Ref.Facing.set(tdir);
+
+                    if (autoFireRof > 0)
+                    {
+                        autoFireRof--;
+                        return;
+                    }
+                    Owner.OwnerObject.Ref.Fire_NotVirtual(currentTarget.OwnerObject.Convert<AbstractClass>(), 0);
+                    autoFireRof = Owner.OwnerObject.Ref.Veterancy.IsElite() ? 2 : 4;
+                }
+            }
+        }
+
+        private int findRate = 0;
+
+        private Pointer<AbstractClass> FindTarget(CoordStruct coord)
+        {
+            if (findRate-- <= 0)
+            {
+                findRate = 3;
+            }
+
+            var zhongli = new[]
+            {
+                "Special",
+                "Neutral"
+            };
+
+            List<Pointer<ObjectClass>> list = ObjectFinder.FindTechnosNear(coord, 7 * Game.CellSize)
+                .Where(x => !x.Ref.Base.GetOwningHouse().Ref.IsAlliedWith(Owner.OwnerObject.Ref.Owner) && !zhongli.Contains(x.Ref.Base.GetOwningHouse().Ref.Type.Ref.Base.ID))
+                .Where(x => Owner.OwnerObject.Ref.CanAttack(x))
+                .OrderBy(x => x.Ref.Base.GetCoords().DistanceFrom(coord))
+                .ToList();
+
+            if (list.Count > 0)
+            {
+                return list.FirstOrDefault().Convert<AbstractClass>();
+            }
+
+            return Pointer<AbstractClass>.Zero;
+        }
+
+        public override void OnDestroy()
+        {
+            EventSystem.General.RemoveTemporaryHandler(EventSystem.General.LogicClassUpdateEvent, AutoFireUpdate);
+        }
+
     }
 }
