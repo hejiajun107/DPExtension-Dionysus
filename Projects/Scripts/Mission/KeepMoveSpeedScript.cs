6eece8d67efe8be2a8b6f4598a4cc2044ae9273e
hejiajun107 <hejiajun107@126.com>
2025-09-26 20:13:54 +0800

    保持移速脚本

---
+++ Projects/Scripts/Mission/KeepMoveSpeedScript.cs
@@ -0,0 +1,71 @@
+﻿using Extension.Ext;
+using Extension.INI;
+using Extension.Script;
+using PatcherYRpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Mission
+{
+    [ScriptAlias(nameof(KeepMoveSpeedScript))]
+    [Serializable]
+    public class KeepMoveSpeedScript : TechnoScriptable
+    {
+        public KeepMoveSpeedScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        private int duration = 100;
+
+        private int targetSpeed = 4;
+
+        private int speed = 4;
+
+        private double initMultiper = 1.0;
+
+        private bool inited = false;
+
+        public override void Awake()
+        {
+            var ini = GameObject.CreateRulesIniComponentWith<KeepMoveSpeedData>(Owner.OwnerObject.Ref.Type.Ref.Base.Base.ID);
+            speed = Owner.OwnerObject.Ref.Type.Ref.Speed;
+            targetSpeed = ini.Data.Speed;
+            duration = ini.Data.Duration;
+
+            base.Awake();
+        }
+
+
+        public override void OnUpdate()
+        {
+            var pfoot = Owner.OwnerObject.Convert<FootClass>();
+            if (!inited) {
+                inited = true;
+                initMultiper = pfoot.Ref.SpeedMultiplier;
+            }
+
+            if (duration-- > 0)
+            {
+                pfoot.Ref.SpeedMultiplier = targetSpeed / speed;
+            }
+            else
+            {
+                pfoot.Ref.SpeedMultiplier = initMultiper;
+                DetachFromParent();
+            }
+        }
+    }
+
+    
+    public class KeepMoveSpeedData:INIAutoConfig
+    {
+        [INIField(Key = "KeepMoveSpeed.Duration")]
+        public int Duration = 100;
+
+        [INIField(Key = "KeepMoveSpeed.Speed")]
+        public int Speed = 4;
+    }
+}
