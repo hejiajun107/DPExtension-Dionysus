c0b61aab57b3fc7ffb91ce29ae227dda123ecf02
hejiajun107 <hejiajun107@126.com>
2025-10-03 13:35:05 +0800

    盟08存档全局数据

---
+++ Projects/Scripts/Mission/All08GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamicPatcher;
+using Extension.Coroutines;
+using Extension.Ext;
+using Extension.Script;
+using Extension.Shared;
+using PatcherYRpp;
+using Scripts;
+
+namespace Scripts
+{
+    [ScriptAlias(nameof(All08GameManager))]
+    [Serializable]
+    public class All08GameManager : TechnoScriptable
+    {
+        public All08GameManager(TechnoExt owner) : base(owner)
+        {
+        }
+
+        private MissionData missionData = new MissionData();
+        public override void Awake()
+        {
+            missionData = MissionDataHelper.Load();
+            //重置08数据
+            missionData.DataAll08 = new DataAll08();
+        }
+
+        public override void OnReceiveDamage(Pointer<int> pDamage, int DistanceFromEpicenter, Pointer<WarheadTypeClass> pWH, Pointer<ObjectClass> pAttacker, bool IgnoreDefenses, bool PreventPassengerEscape, Pointer<HouseClass> pAttackingHouse)
+        {
+            if (pWH.IsNull)
+                return;
+
+            if(pWH.Ref.Base.ID == "MSSIGWh1")
+            {
+                if(missionData.DataAll08.FindTeams < 3)
+                {
+                    missionData.DataAll08.FindTeams++;
+                }
+            }
+            else if(pWH.Ref.Base.ID == "MSSIGWh2")
+            {
+                Owner.GameObject.StartCoroutine(GiveMoneyAfter(50, 2000));
+            }
+            else if (pWH.Ref.Base.ID == "MSSIGWh3")
+            {
+                Owner.GameObject.StartCoroutine(GiveMoneyAfter(50, 2000));
+            }
+            else if (pWH.Ref.Base.ID == "MSSIGWh4")
+            {
+                missionData.DataAll08.AirForceActited = true;
+            }
+            else if(pWH.Ref.Base.ID == "MSSIGWh5")
+            {
+                missionData.DataAll08.Cash = Owner.OwnerObject.Ref.Owner.Ref.Available_Money();
+                MissionDataHelper.Save(missionData);
+            }
+
+        }
+
+        IEnumerator GiveMoneyAfter(int delay,int amount)
+        {
+            yield return new  WaitForFrames(delay);
+            Owner.OwnerObject.Ref.Owner.Ref.TransactMoney(2000);
+        }
+    }
+}
---
+++ Projects/Scripts/Shared/MissionData.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension.Shared
+{
+    public class MissionDataHelper
+    {
+        public static void Save(MissionData data)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(MISSION_SAVE_PATH, false))
+                {
+                    var json = JsonConvert.SerializeObject(data);
+                    var str = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(json));
+                    sw.Write(str);
+                }
+            }
+            catch (Exception) { }   
+        }
+
+        private const string MISSION_SAVE_PATH = "./Client/mission_saved";
+
+        public static MissionData Load()
+        {
+            if(File.Exists(MISSION_SAVE_PATH))
+            {
+                try
+                {
+                    using (StreamReader sw = new StreamReader(MISSION_SAVE_PATH))
+                    {
+                        var str = sw.ReadToEnd();
+                        byte[] decodedBytes = Convert.FromBase64String(str);
+                        string decodedString = Encoding.UTF8.GetString(decodedBytes);
+
+                        return JsonConvert.DeserializeObject<MissionData>(decodedString) ?? new MissionData();
+                    }
+                }catch(Exception ex)
+                {
+                    return new MissionData();
+                }
+            }
+            else
+            {
+                return new MissionData();
+            }
+        }
+    }
+
+    [Serializable]
+    public class MissionData
+    {
+        public DataAll08 DataAll08 { get; set; } = new DataAll08();
+    }
+
+    [Serializable]
+    public class DataAll08
+    {
+        /// <summary>
+        /// 拥有金钱
+        /// </summary>
+        public int Cash { get; set; } = 1;
+
+        /// <summary>
+        /// 找到的小队数
+        /// </summary>
+        public int FindTeams { get; set; } = 0;
+
+        /// <summary>
+        /// 是否触发空中支援
+        /// </summary>
+        public bool AirForceActited { get; set; } = false;
+    }
+}
