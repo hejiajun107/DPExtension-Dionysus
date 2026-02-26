6b2be5268a9b790eab85890eb4b654b4690dbf16
hejiajun107 <hejiajun107@126.com>
2026-02-26 22:07:48 +0800

    思金工程师

---
+++ Projects/Scripts/Scrin/SCEngineerScript.cs
@@ -0,0 +1,38 @@
+﻿using DpLib.Scripts;
+using Extension.Ext;
+using Extension.Script;
+using PatcherYRpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Scrin
+{
+    [Serializable]
+    [ScriptAlias(nameof(SCEngineerScript))]
+    public class SCEngineerScript : TechnoScriptable
+    {
+        public SCEngineerScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        private int rof = 200;
+
+        public override void OnUpdate()
+        {
+            if (rof-- > 0)
+            {
+                return;
+            }
+            rof = 200;
+
+            if (Owner.OwnerObject.Ref.Base.IsAlive && Owner.OwnerObject.Ref.Base.IsOnMap && Owner.OwnerObject.Ref.Base.InLimbo == false)
+            {
+                Pointer<BulletClass> pBullet = BulletTypeClass.ABSTRACTTYPE_ARRAY.Find("Invisible").Ref.CreateBullet(Owner.OwnerObject.Convert<AbstractClass>(), Owner.OwnerObject, 30, WarheadTypeClass.ABSTRACTTYPE_ARRAY.Find("SCEngineerWh"), 100, false);
+                pBullet.Ref.DetonateAndUnInit(Owner.OwnerObject.Ref.Base.Base.GetCoords());
+            }
+        }
+    }
+}
